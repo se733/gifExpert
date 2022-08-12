12e148c63d0c47c3257a356a1230b829fb6878d1
se733 <chavezsebas737@gmail.com>
2022-08-12 11:39:27 -0300

    creación proyecto

---
+++ src/GifExpert.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const GifExpert = () => {
+  return (
+    <h1>GifExpert</h1>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GifExpert } from './GifExpert'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GifExpert />
+  </React.StrictMode>
+)
