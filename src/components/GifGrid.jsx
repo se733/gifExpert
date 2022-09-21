a78b7a606f9c79062e71fed9f85ea355377d5f23
se733 <chavezsebas737@gmail.com>
2022-09-21 12:24:01 -0300

    agregado gifItem y custom hook

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,12 +1,29 @@
-import {getGifs} from '../helpers/getGifs'
+import { useState } from "react";
+import { useEffect } from "react";
+import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
 
-export const GifGrid = ({category}) => {
-  
-    getGifs(category)
-  
+export const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
   return (
-        <>
-            <h3>{category}</h3>
-        </>
-    )
-}
+    <>
+      <h3>{category}</h3>
+
+      <div className="card-grid">
+        {images.map((image) => (
+          <GifItem key={image.id} {...image} />
+        ))}
+      </div>
+    </>
+  );
+};
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,13 +1,14 @@
 export const getGifs = async(category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=XgCe1pk2CMqaspISXasbja29m9h69ZUU&q=${category}&limit=20`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=XgCe1pk2CMqaspISXasbja29m9h69ZUU&q=${category}&limit=10`
 
     const resp = await fetch(url)
     const {data} = await resp.json()
 
     const gifs = data.map(img => ({
         id: img.id,
-        tittle: img.title,
+        title: img.title,
         url: img.images.downsized_medium.url
     }))
     console.log(gifs);
+    return gifs;
 }
