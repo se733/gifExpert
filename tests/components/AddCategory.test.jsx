36d8b1b268eb6adf8b3abcd2d1833b3d51e9b556
se733 <chavezsebas737@gmail.com>
2022-10-18 12:51:28 -0300

    pruebas terminadas en AddCategory y comenzadas en GifGrid

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -8,7 +8,35 @@ describe("Pruebas en AddCategory", () => {
     fireEvent.input(input, { target: { value: "saitama" } });
 
     expect(input.value).toBe("saitama");
+  });
 
-    test('Debe llamar onNewCategory si el input tiene un valor')
+  test("Debe llamar onNewCategory si el input tiene un valor", () => {
+    const inputValue = "saitama";
+    //funcion ficticia de onNewCategory
+    const onNewCategory = jest.fn();
+
+    render(<AddCategory onNewCategory={onNewCategory} />);
+
+    //referencias al input y formulario
+    const input = screen.getByRole("textbox");
+    const form = screen.getByRole("form");
+
+    //disparando los eventos
+    fireEvent.input(input, { target: { value: inputValue } });
+    fireEvent.submit(form);
+
+    expect(input.value).toBe("");
+    //toHaveBeenCalledWith es para que la funcion sea llamada con dicho valor
+    expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+  });
+
+  test("No debe llamar el onNewCategory si el input está vacio", () => {
+    const onNewCategory = jest.fn();
+    render(<AddCategory onNewCategory={onNewCategory} />);
+
+    const form = screen.getByRole("form");
+    fireEvent.submit(form);
+
+    expect(onNewCategory).toHaveBeenCalledTimes(0);
   });
 });
