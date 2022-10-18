fc218bf32961f12469344bb0aa4eda57ab9591a8
se733 <chavezsebas737@gmail.com>
2022-10-18 16:44:20 -0300

    test sobre hook useFetchGifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { GifItem } from "./GifItem";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 
@@ -7,7 +8,7 @@ export const GifGrid = ({ category }) => {
   return (
     <>
       <h3>{category}</h3>
-
+      {isLoading && <h2>Cargando...</h2>}
       <div className="card-grid">
         {images.map((image) => (
           <GifItem key={image.id} {...image} />
@@ -15,4 +16,8 @@ export const GifGrid = ({ category }) => {
       </div>
     </>
   );
+};
+
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
 };
--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,3 +1,46 @@
-describe('Pruebas en GifGrid', () => { 
-        
- })
+import { render, screen } from "@testing-library/react";
+import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock("../../src/hooks/useFetchGifs");
+
+describe("Pruebas en GifGrid", () => {
+  const category = "one punch";
+
+  test("debe mostrar el loading inicialmente", () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: false,
+    });
+
+    render(<GifGrid category={category} />);
+    // expect(screen.getByText("Cargando..."));
+    expect(screen.getByText(category));
+    screen.debug();
+  });
+
+  test("debe mostrar items cuando se cargan las imagenes", () => {
+    const gifs = [
+      {
+        id: "ABC",
+        title: "Saitama",
+        url: "https://localhost/saitama.jpg",
+      },
+      {
+        id: "CBA",
+        title: "Goku",
+        url: "https://localhost/saitama.jpg",
+      },
+    ];
+
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false,
+    });
+
+    render(<GifGrid category={category} />);
+    expect(screen.getAllByRole('img').length).toBe(2)
+    expect(screen.getAllByText('')).toBeTruthy();
+
+});
+});
